cb538339b72e8684b435f020ddf2a5e27604e91f
euglazer <eugenestapler@gmail.com>
2016-03-14 18:28:04 -0700

    relative path to stl

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = function (THREE) {
   var STLLoader = require('three-stl-loader')(THREE)
   var loader = new STLLoader()
-  var stlPath = './dolphin.stl'
+  var stlPath = __dirname + '/dolphin.stl'
 
   return function (onLoad) {
     loader.load(stlPath, onLoad)
